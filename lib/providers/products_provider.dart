4388f8e4e735b600e3f5c1f4d4ff50c07ab1d252
Andrew Marfo <andrewmarfo23@gmail.com>
2025-05-03 12:39:38 +0000

    set up the products list in the provider

---
+++ lib/providers/products_provider.dart
@@ -0,0 +1,52 @@
+import 'package:ecommerce_riverpod/models/product.dart';
+
+const List<Product> allProducts = [
+  Product(
+    id: '1',
+    title: "shorts",
+    price: 12,
+    image: "assets/products/shorts.png",
+  ),
+  Product(
+    id: "2",
+    title: "karati kit",
+    price: 34,
+    image: "assests/products/karati.png",
+  ),
+  Product(
+    id: "3",
+    title: "Denim Jeans",
+    price: 64,
+    image: "assests/products/jeans.png",
+  ),
+  Product(
+    id: "4",
+    title: "Red Backpack",
+    price: 14,
+    image: "assests/products/backpack.png",
+  ),
+  Product(
+    id: "5",
+    title: "Drum and Stick",
+    price: 29,
+    image: "assests/products/drum.png",
+  ),
+  Product(
+    id: "6",
+    title: "Blue Suitcase",
+    price: 44,
+    image: "assests/products/suitcase.png",
+  ),
+  Product(
+    id: "7",
+    title: "Roller Skates",
+    price: 52,
+    image: "assests/products/skates.png",
+  ),
+  Product(
+    id: "8",
+    title: "Electric Guitar",
+    price: 79,
+    image: "assests/products/guitar.png",
+  ),
+];
